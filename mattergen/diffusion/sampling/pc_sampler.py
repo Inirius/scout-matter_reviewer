0195e87363c780e987369c27c705a81801f13376
Auguste de Lambilly <auguste.delam@yahoo.fr>
2026-09-11 00:36:00 +0200

    Correction of the forward corruption, batch was missing

--- mattergen/diffusion/sampling/pc_sampler.py
+++ mattergen/diffusion/sampling/pc_sampler.py
@@ -295,12 +295,15 @@ class PredictorCorrector(Generic[Diffusable]):
         s: torch.Tensor,
         k: str,
         batch_idx: torch.Tensor | None = None,
+        batch: Diffusable | None = None,
     ) -> Tuple[Diffusable, torch.Tensor]:
         """Forward pass for a corruption from s to t."""
         return (
-            self._multi_corruption.corruptions[k].sample_from_s(batch_k, t, s, batch_idx=batch_idx),
+            self._multi_corruption.corruptions[k].sample_from_s(
+                batch_k, t, s, batch_idx=batch_idx, batch=batch
+            ),
             self._multi_corruption.corruptions[k].marginal_prob_from_s(
-                batch_k, t, s, batch_idx=batch_idx
+                batch_k, t, s, batch_idx=batch_idx, batch=batch
             )[0],
         )
 
@@ -423,7 +426,7 @@ class PredictorCorrector(Generic[Diffusable]):
                 samples_means = apply(
                     fns=fns,
                     batch_k=batch_,
-                    broadcast={"t": t, "s": t + dt},
+                    broadcast={"t": t, "s": t + dt, "batch": batch_},
                     k={u: u for u in self._multi_corruption.corrupted_fields if u in batch_},
                     batch_idx=self._multi_corruption._get_batch_indices(batch_),
                 )
